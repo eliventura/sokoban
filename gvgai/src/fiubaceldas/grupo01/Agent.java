1f35062c8da80d41ef7620c1e012f2906ee3c627
Eli <Eli@Eliana>
2016-11-03 15:47:29 -0300

    s

---
+++ gvgai/src/fiubaceldas/grupo01/Agent.java
@@ -0,0 +1,20 @@
+package fiubaceldas.grupo01;
+
+import ontology.Types.ACTIONS;
+import tools.ElapsedCpuTimer;
+import core.game.StateObservationMulti;
+import core.player.AbstractMultiPlayer;
+
+public class Agent extends AbstractMultiPlayer {
+
+	public Agent(StateObservationMulti stateObs, ElapsedCpuTimer elapsedTimer, int playerID) {
+	}
+	
+	@Override
+	public ACTIONS act(StateObservationMulti stateObs,
+			ElapsedCpuTimer elapsedTimer) {
+		// TODO Auto-generated method stub
+		return ACTIONS.ACTION_NIL;
+	}
+
+}
